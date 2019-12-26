6f6b47127e40808e470a70fc7b137099d502e279
יקט <>
2019-12-26 17:36:46 +0200

    Create MASTER_INVERTED constant

--- src/main/java/robot/Constants.java
+++ src/main/java/robot/Constants.java
@@ -61,6 +61,10 @@ public class Constants {
         }
     }
 
+
+    public static class Gripper {
+        public static final boolean MASTER_INVERTED = false;
+    }
 }
 
 //Anything in this class will replace the original constants when boolean is true
