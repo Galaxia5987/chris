d9a64b87a732225557b8f3de7b533bf9c4e00b1c
lior <lior5858@gmail.com>
2019-12-26 18:04:27 +0200

    Create ports for the climb system

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -1,12 +1,19 @@
 package robot;
 
+import static org.apache.commons.lang3.ObjectUtils.CONST;
+
 /**
  * A class holding all of the ports on the robot.
  * Place mechanism-specific ports inside their own sub-class.
  * When accessing a mechanism-specific port, call Ports.[MECHANISM].[PORT_NAME]
  */
 public class Ports {
-    public static class ExampleSubsystem1 {
+
     public static class ClimbSubsystem {
+        public static final int CLIMB_MASTER = CONST(0);
+        public static final int CLIMB_SLAVE = CONST(0);
+        public static final int RAIL_MOTOR = CONST(0);
     }
+
+
 }
