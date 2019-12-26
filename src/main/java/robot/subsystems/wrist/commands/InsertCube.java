0577609cc21c1aef94fda15cb7b2810052e2bc34
יקט <>
2019-12-26 17:36:10 +0200

    Create Insert Cube command

---
+++ src/main/java/robot/subsystems/wrist/commands/InsertCube.java
@@ -0,0 +1,46 @@
+package robot.subsystems.wrist.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+import static robot.Robot.gripper;
+
+public class InsertCube extends Command {
+    private double speed;
+    private double timeout;
+    private Timer timer = new Timer();
+
+    public InsertCube(double speed, double timeout) {
+        requires(gripper);
+        this.speed = speed;
+        this.timeout = timeout;
+    }
+
+    public InsertCube(double speed) {
+        this(speed, 0);
+    }
+
+    @Override
+    protected void initialize() {
+        timer.reset();
+        timer.start();
+        gripper.setSpeed(speed);
+    }
+
+    @Override
+    protected void execute() {
+        gripper.setSpeed(speed);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return timeout > 0 && timer.get() > timeout;
+    }
+
+    @Override
+    protected void end() {
+        timer.stop();
+        gripper.setSpeed(0);
+    }
+
+}
