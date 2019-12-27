e26cf6dd7c2ccf6ecfd58ae0b48fe1fd9b8e02d7
oreld <orel.david1502@gmail.com>
2019-12-27 10:36:54 +0200

    Document the subsystem

--- src/main/java/robot/slingshot/slingshotSubsystem.java
+++ src/main/java/robot/slingshot/slingshotSubsystem.java
@@ -12,7 +12,7 @@ import robot.Constants;
 import robot.Ports;
 
 /**
- * An example subsystem to represent the basic structure of a robot subsystem
+ * The subsystem of the slingshot which will be used for shooting the cargo to the target
  */
 public class slingshotSubsystem extends Subsystem {
     private TalonSRX slingMaster = new TalonSRX(Ports.slingshot.SLING_MASTER);
@@ -31,38 +31,72 @@ public class slingshotSubsystem extends Subsystem {
         slingMaster.configForwardLimitSwitchSource(LimitSwitchSource.FeedbackConnector, LimitSwitchNormal.NormallyOpen);
     }
 
+    /**
+     * The function to convert ticks to meters
+     * @param ticks the ticks from the encoder
+     * @return the distance in meters
+     */
     public double convertTickToMeters(double ticks){
         return ticks/Constants.slingshot.TICKS_PER_METER;
     }
 
+    /**
+     * The function to convert meters to ticks
+     * @param meters the distance in meters
+     * @return the ticks of the encoder
+     */
     public double convertMetersToTicks(double meters){
         return meters*Constants.slingshot.TICKS_PER_METER;
     }
 
+    /**
+     * This method would shift to a neutral gear
+     */
     public void shiftToNeutral(){
         shifter.set(DoubleSolenoid.Value.kReverse);
     }
 
+    /**
+     * This method would shift and connect the gear
+     */
     public void shiftToConnect(){
         shifter.set(DoubleSolenoid.Value.kForward);
     }
 
+    /**
+     *
+     * @return the position of the spring in meters
+     */
     public double getPosition(){
         return convertTickToMeters(slingMaster.getSelectedSensorPosition());
     }
 
+    /**
+     * That method would place the spring in a certain position
+     * @param position the position of the spring in meters
+     */
     public void setPosition(double position){
         slingMaster.set(ControlMode.Position, convertMetersToTicks(position));
     }
 
+    /**
+     * That method would move the spring a certain distance
+     * @param distance the distance that the motors would move the spring in meters
+     */
     public void loadByDistance(double distance){
         slingMaster.set(ControlMode.Position, convertMetersToTicks(getPosition() + distance));
     }
 
+    /**
+     * This method would reset the encoder
+     */
     public void Reset(){
         slingMaster.setSelectedSensorPosition(0);
     }
 
+    /**
+     * If the sling would reach to the limit switch it would reset the encoder and connect the motor to the gear
+     */
     public void update(){
         if (slingMaster.getSensorCollection().isFwdLimitSwitchClosed()){
             Reset();
