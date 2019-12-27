da1202114d460c2b3c01088051be66507da9019b
Barel Shlidor <barel@shlidor.org>
2019-12-27 10:48:43 +0200

    Create empty methods for  the Wrist subsystem

--- src/main/java/robot/subsystems/wrist/Wrist.java
+++ src/main/java/robot/subsystems/wrist/Wrist.java
@@ -1,10 +1,34 @@
 package robot.subsystems.wrist;
 
-import edu.wpi.first.wpilibj.command.Command;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import static robot.Ports.Wrist.*;
+
 public class Wrist extends Subsystem {
+    private TalonSRX masterMotor = new TalonSRX(MASTER);
+    private VictorSPX slaveMotor = new VictorSPX(SLAVE);
+
+    public Wrist() {
+
+        slaveMotor.follow(masterMotor);
+    }
+
     @Override
     protected void initDefaultCommand() {
+    }
+
+    public void turnTo(double angle) {
+
+    }
+
+    public void turnTo(State state) {
+
+    }
+
+
+    public enum State {
+
     }
 }
