8f83a6a0bbfdb2996a6a39db40be1f43b8b9e432
Adam <adam.vichik@gmail.com>
2019-12-27 10:04:44 +0200

    Create speed and timeout in PullBall constructor

--- src/main/java/robot/subsystems/examplesubsystem/intake/commands/PullBall.java
+++ src/main/java/robot/subsystems/examplesubsystem/intake/commands/PullBall.java
@@ -1,14 +1,20 @@
 package robot.subsystems.examplesubsystem.intake.commands;
 
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
+import static robot.Robot.intake;
 
 public class PullBall extends Command {
     private Timer timer = new Timer();
+    private double speed;
+    private double timeout;
 
-    public PullBall(){
-
+    public PullBall(double speed, double timeout){
+        this.speed = speed;
+        this.timeout = timeout;
+        requires(intake);
     }
 
     @Override
