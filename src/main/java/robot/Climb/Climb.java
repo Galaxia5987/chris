6d0ea0e6b5f8e2d7c8ef458b4fc0b4b46d928e97
lior <lior5858@gmail.com>
2019-12-29 15:14:11 +0200

    Configure motionmagic velocity and acceleration

--- src/main/java/robot/Climb/Climb.java
+++ src/main/java/robot/Climb/Climb.java
@@ -30,6 +30,9 @@ public class Climb extends Subsystem {
         climbSlave.setInverted(Constants.ClimbSubsystem.CLIMB_SLAVE_INVERTED);
         railMotor.setInverted(Constants.ClimbSubsystem.RAIL_MOTOR_INVERTED);
 
+        climbMaster.configMotionCruiseVelocity(convertMetersToTicks(Constants.ClimbSubsystem.CRUISE_VELOCITY), Constants.TALON_TIME_OUT_MS);
+        climbMaster.configMotionAcceleration(convertMetersToTicks(Constants.ClimbSubsystem.CRUISE_ACCELERATIONN), Constants.TALON_TIME_OUT_MS)
+
         //Encoder invertion
         climbMaster.setSensorPhase(Constants.ClimbSubsystem.CLIMB_ENCODER_INVERTED);
 
